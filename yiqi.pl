70c2fa9bf11a107252cb40fcfc735b4cb3992993
Михаил Дарьин <darin.m@tvzavr.ru>
2018-04-18 19:53:10 +0300

    сформированы генераторы для файла api теста

--- yiqi.pl
+++ yiqi.pl
@@ -11,7 +11,7 @@
 # 
 ##
 use warnings;
-use strict;
+#use strict;
 use File::Basename qw(basename dirname);
 use File::Spec;
 use Getopt::Long;
@@ -37,6 +37,9 @@ GetOptions("module=s" => \$options{"module"},
 
 print $options{"module"} . "\n";
 
+print basename "move/to" . "\n";
+
+
 # получить список обработчиков
 # Файл модуля тестирования
 #	CARGOROOT/test/<module>_SUITE.erl
@@ -57,8 +60,41 @@ print $options{"module"} . "\n";
 # Алгоритм
 #	открыть файл
 # вывести заголовок
-#	сгенеировать функци API к тестируемому модулю
+&generate_t_mod_head(stdout, "yiqi_test_mod");
+#	сгенеировать функци API к тестируемому модулoю
+&generate_t_mod_fun_clause(stdout, "yiqi_test_mod", "yiqi_test_topic");
 # зкрыть файл
+sub generate_t_mod_head {
+	my($fout, $module) = @_;
+	print $fout "%%%%----------------------------------------------\n";
+	print $fout "%%% Module: API $module module for test suite\n";
+	print $fout "%%% Desc: \n";
+	print $fout "%%% Author:\n";
+	print $fout "%%% Autogen: Yi qi\n";
+	print $fout "%%% wiki page -> [link to wiki or page name]\n";
+	print $fout "%%%%----------------------------------------------\n";
+	print $fout "-module(t_$module).\n";
+	print $fout "-compile([export_all]).\n";
+	print $fout "\n";
+	print $fout "% Команды управления подсистемой Добавьте название\n";
+	print $fout "\n";
+}
+
+sub generate_t_mod_fun_clause {
+	my($fout, $module, $topic) = @_;
+	print $fout "% MQTT pub:  /<CID>/$module/$topic\n";
+	print $fout "% MQTT sub:  /<CID>/$module/res/$topic\n";
+	print $fout "\$function(Mqtt) -> %TODO: Довьте требуетмые аргументы фукнции\n";
+	print $fout "	Outgoing = <<\"$module/$topic\">>,\n";
+	print $fout "	Incomeing = <<\"$module/res/$topic\">>,\n";
+	print $fout "	tst:sub(Mqtt, Incomeing),\n";
+	print $fout "	Payload = [\n";
+	print $fout "		%TODO:insert your data here as a proplilst {key, Value} pairs\n";
+	print $fout "	],\n";
+	print $fout "	Reply = tst:pubr(Mqtt, Outgoing, Payload),\n";
+	print $fout "	ct:log(\"::reply -> ~p~n\", [Reply]),\n";
+	print $fout "	Reply.\n";
+}
 
 # Файл модуля обработчика каналов, реализующего функционал подсистемы
 #	CARGOROOT/lib/<module>.erl
