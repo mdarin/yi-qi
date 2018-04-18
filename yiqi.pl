578e49367dd6be284610bec1bde40309bfc8549e
Михаил Дарьин <darin.m@tvzavr.ru>
2018-04-18 17:37:52 +0300

    added project files

---
+++ yiqi.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl -w
+#** ------------------------------------------------------------------
+# nam: Yi qi - Template generator for CargoNet project 
+# vsn: 0.0.1
+# dsc: This is a devtool  
+# crt: Ср апр 18 17:36:10 MSK 2018
+# upd: 
+# ath: Michael DARIN, Moscow, Russia, (c) 2018
+# lic: gnu>=2, "AS-IS", "NO WARRENTY"
+# cnt: darin.m@tvzavr.ru
+# 
+##
+use warnings;
+use strict;
+use File::Basename qw(basename dirname);
+use File::Spec;
+use Getopt::Long;
+use autodie;
+
+print "hello yiqi\n";
+
